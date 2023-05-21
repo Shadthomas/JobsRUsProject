3f4facaff42bd6b2e73a88608e170eac33880ce7
shadthomas91 <110137366+Shadthomas@users.noreply.github.com>
2023-05-20 19:55:46 -0500

    Updating Project

---
+++ src/com/skillstorm/project/Post.java
@@ -0,0 +1,86 @@
+package com.skillstorm.project;
+
+public class Post {
+
+		private String id;
+		private String title;
+		private String description;
+		private String address;
+		private String city;
+		private String state;
+		private String zip;
+		private String Salary;
+		private String field;
+		private String email;
+		
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		
+		public String getState() {
+			return state;
+		}
+		public void setState(String state) {
+			this.state = state;
+		}
+		
+		public String getZip() {
+			return zip;
+		}
+		public void setZip(String zip) {
+			this.zip = zip;
+		}
+		
+		public String getSalary() {
+			return Salary;
+		}
+		public void setSalary(String salary) {
+			Salary = salary;
+		}
+		
+		public String getField() {
+			return field;
+		}
+		public void setField(String field) {
+			this.field = field;
+		}
+		
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		
+}
---
+++ src/com/skillstorm/project/Resume.java
@@ -0,0 +1,103 @@
+package com.skillstorm.project;
+
+public class Resume {
+	private String id;
+	private String title;
+	private String name;
+	private String address;
+	private String email;
+	private String mobile;
+	private String degree;
+	private String skills;
+	private String workExp;
+	private String projects;
+	private String reference;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getMobile() {
+		return mobile;
+	}
+	
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+	public String getDegree() {
+		return degree;
+	}
+	
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+	
+	public String getSkills() {
+		return skills;
+	}
+	
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+	
+	public String getWorkExp() {
+		return workExp;
+	}
+	
+	public void setWorkExp(String workExp) {
+		this.workExp = workExp;
+	}
+	
+	public String getProjects() {
+		return projects;
+	}
+	
+	public void setProjects(String projects) {
+		this.projects = projects;
+	}
+	
+	public String getReference() {
+		return reference;
+	}
+	
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+	
+}
